15c498d5a99def3e03ba96853c5cc091aa2f8f0a
liangjk <liang_jk@ruc.edu.cn>
2018-06-11 22:35:04 +0800

    Resources

---
+++ client/Resources/map/Map_new/river3.tsx
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="river3" tilewidth="32" tileheight="32" tilecount="48" columns="8">
+ <image source="C:/Users/92127/Downloads/2d野外河滩溪流场景素材-1_爱给网_aigei_com.png" width="279" height="223"/>
+ <terraintypes>
+  <terrain name="沙地" tile="11"/>
+ </terraintypes>
+ <tile id="11" terrain="0,0,0,0"/>
+</tileset>
